55f341090441edaf4e2e8fa6c01bfba3d23b5176
thinklancer <thinklancer@gmail.com>
2013-03-15 00:51:13 +0100

    finish quiz 6

--- hw1.py
+++ hw1.py
@@ -20,25 +20,32 @@ def viterbi(str,model):
         for u in fullset:
             for v in fullset:
                 pimax = 0
+                tw = ''
                 for w in fullset:
                     tmp = pi[(k-1,w,u)]*model.ngram2[(w,u,v)]*model.emission[(str[k-1],v)]
-                    #print k,w,u,v,tmp,str[k-1],model.ngram2[(w,u,v)],model.emission[(str[k-1],v)]
-                    if tmp >= pimax:
+                    if tmp > pimax:
                         pimax = tmp
                         tw = w
-                pi[(k+1,u,v)] = pimax
-                bp[(k+1,u,v)] = w
-    # back construction
+                pi[(k,u,v)] = pimax
+                bp[(k,u,v)] = tw
     pimax = 0
     for u in fullset:
         for v in fullset:
-            tmp = pi[(n+1,u,v)]*model.ngram2[(u,v,'STOP')]
-            print u,v,pi[(n+1,u,v)],model.ngram2[(u,v,'STOP')]
+            tmp = pi[(n,u,v)]*model.ngram2[(u,v,'STOP')]
             if tmp > pimax:
                 tu = u
                 tv = v
                 pimax = tmp
-    print tu,tv,pimax
+
+    # back construction
+    state.append(tv)
+    state.append(tu)
+    for k in range(n-2,0,-1):
+        state.append(bp[(k+2,state[-1],state[-2])])
+        
+    print 'max likelihood:',pimax        
+    print str
+    print state[::-1]
             
 
 def qz6():
@@ -47,10 +54,13 @@ def qz6():
     input = file("qz6.counts",'r')
     model.read_counts(input)
     model.processing()
+    # solve the problem
     str = 'the cat saw the saw'.split(" ")
-    print model.ngram2
     viterbi(str,model)
+
+def qz7():
+    
     
 if __name__ == "__main__":
-    qz6()
+    qz7()
     
